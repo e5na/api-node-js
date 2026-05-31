12d66e39b7826b49cc1603df6cb38f3a113258d2
marko <marko.esna@gmail.com>
2026-05-31 16:41:10 +0300

    change: delete all user entries before checking

--- tests/empty-array.spec.ts
+++ tests/empty-array.spec.ts
@@ -1,9 +1,22 @@
 import { test, expect } from '@playwright/test';
-import { getAllUsers } from "./helpers/api-user-helper";
+import {deleteUser, getAllUsers} from "./helpers/api-user-helper";
 
 export let baseURL: string = 'http://localhost:3000/users';
 
 test('all users: should return empty array when no users', async ({ request }) => {
-    const allUsers = await getAllUsers(request);
-    expect(await allUsers.text()).toBe('[]');
+    const response = await getAllUsers(request);
+    const currentUsers = await response.json();
+
+    if (currentUsers.length === 0) {
+        expect(currentUsers).toEqual([]);
+        return;
+    }
+
+    for (const user of currentUsers) {
+        await deleteUser(request, user.id);
+    }
+
+    const finalResponse = await getAllUsers(request);
+    const allUsers = await finalResponse.json();
+    expect(allUsers).toEqual([]);
 });
