cce3fb0647a1fa8ce9bd09bae1bf062a897907de
marko <marko.esna@gmail.com>
2026-05-31 15:55:52 +0300

    feat: added helpers and tests

---
+++ tests/empty-array.test.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { getAllUsers } from "./helpers/api-user-helper";
+
+export let baseURL: string = 'http://localhost:3000/users';
+
+test.describe('User management API 2', () => {
+
+    test('all users: should return empty array when no users', async ({ request }) => {
+        const allUsers = await getAllUsers(request);
+        expect(await allUsers.text()).toBe('[]');
+    });
+});
