c4759ee613e19fce798d76417b8b221c81bb1dab
marko <marko.esna@gmail.com>
2026-06-01 11:02:21 +0300

    feat: add users deletion before tests

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -7,6 +7,47 @@ export let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API', () => {
 
+    test.beforeEach(async ({ request }) => {
+        // get all users
+        const response = await request.get(`${baseURL}`);
+        const responseBody = await response.json()
+        // get the number of objects in the array returned
+        const numberOfObjects = responseBody.length;
+
+        // create an empty array to store all user ID
+        let userIDs = [];
+
+        // loop through all users and store their ID in an array
+        for (let i = 0; i < numberOfObjects; i++) {
+            // get user ID from the response
+            let userID = responseBody[i].id;
+            // push is used to add elements to the end of an array
+            userIDs.push(userID);
+        }
+
+        // delete all users in a loop using previously created array
+        for (let i = 0; i < numberOfObjects; i++) {
+            // delete user by id
+            let response = await request.delete(`${baseURL}/${userIDs[i]}`);
+            // validate the response status code
+            expect.soft(response.status()).toBe(200);
+        }
+
+        // verify that all users are deleted
+        const responseAfterDelete = await request.get(`${baseURL}`);
+        expect(responseAfterDelete.status()).toBe(200);
+        const responseBodyEmpty = await responseAfterDelete.text()
+        // validate that the response is an empty array
+        expect(responseBodyEmpty).toBe('[]');
+    })
+
+    test('all users: should return empty array when no users', async ({ request }) => {
+        const response = await request.get(`${baseURL}`);
+        expect(response.status()).toBe(200);
+        const responseBody = await response.text()
+        expect(responseBody).toBe('[]');
+    });
+
     test('find user: should return a user by ID', async ({ request }) => {
         const user = await createUser(request);
         const newUser = await getUserById(request, user.id);
